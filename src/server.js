d78ecf97601f4844e21fe0361cff024345766706
Allan Amaral <allanmaral@hotmail.com>
2019-10-04 01:57:38 -0300

    Added cors and thumbnail virtual thumbnail url

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
+const cors = require ('cors');
 const routes = require('./routes'); 
+const path = require('path');
 
 require('dotenv/config');
 
@@ -11,7 +13,9 @@ mongoose.connect(process.env.MONGO_DB_URI, {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 
 app.listen(3333);
